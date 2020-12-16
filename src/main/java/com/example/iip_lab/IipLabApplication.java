eb28abcaaff83a345a2a933137ab3289a6b46f2a
junhoy00n <jhyoon6257@gmail.com>
2020-12-16 15:54:35 +0900

    backend initialize

---
+++ src/main/java/com/example/iip_lab/IipLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.iip_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IipLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IipLabApplication.class, args);
+	}
+
+}
