10ba7c4239726ba4143825240fe2e1aa31f39a66
junhoy00n <jhyoon6257@gmail.com>
2020-12-17 11:13:22 +0900

    게시글 삭제 구현

--- src/main/java/com/example/iip_lab/controller/BoardController.java
+++ src/main/java/com/example/iip_lab/controller/BoardController.java
@@ -4,10 +4,7 @@ import com.example.iip_lab.dto.BoardDto;
 import com.example.iip_lab.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -59,6 +56,12 @@ public class BoardController {
     @PutMapping("/post/edit/{id}")
     public String update(BoardDto boardDto) {
         boardService.savePost(boardDto);
+        return "redirect:/list";
+    }
+
+    @DeleteMapping("/post/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        boardService.deletePost(id);
         return "redirect:/list";
     }
 }
