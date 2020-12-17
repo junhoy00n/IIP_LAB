a8d63b4e4be4333d6bde45e34dd8d1a7c25cd3f4
junhoy00n <jhyoon6257@gmail.com>
2020-12-17 11:10:06 +0900

    게시글 수정 구현

--- src/main/java/com/example/iip_lab/controller/BoardController.java
+++ src/main/java/com/example/iip_lab/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.List;
 
@@ -46,5 +47,18 @@ public class BoardController {
         BoardDto boardDto = boardService.getPost(id);
         model.addAttribute("post", boardDto);
         return "board/detail.html";
+    }
+
+    @GetMapping("/post/edit/{id}")
+    public String edit(@PathVariable("id") Long id, Model model) {
+        BoardDto boardDto = boardService.getPost(id);
+        model.addAttribute("post", boardDto);
+        return "board/edit.html";
+    }
+
+    @PutMapping("/post/edit/{id}")
+    public String update(BoardDto boardDto) {
+        boardService.savePost(boardDto);
+        return "redirect:/list";
     }
 }
