d8641f5cef698360504f84a8565110c501881ca5
junhoy00n <jhyoon6257@gmail.com>
2020-12-18 11:11:55 +0900

    roll back

--- src/main/java/com/example/iip_lab/domain/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.iip_lab.domain.repository;
-
-import com.example.iip_lab.domain.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
--- src/main/java/com/example/iip_lab/domain/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.iip_lab.domain.repository;
-
-import com.example.iip_lab.domain.entity.File;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<File, Long> {
-}
