ed58a967a884fe7873c21d458e76000bc757abae
junhoy00n <jhyoon6257@gmail.com>
2020-12-21 13:16:04 +0900

    backend initialize

---
+++ src/main/java/com/iip/lab_page/LabPageApplication.java
@@ -0,0 +1,13 @@
+package com.iip.lab_page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iip/lab_page/LabPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iip.lab_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
